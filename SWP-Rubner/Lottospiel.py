bc1091af4e1566836de607ab981c1d7a2b6db946
Moert52 <mcetinkaya@tsn.at>
2022-10-18 20:48:23 +0200

    SWP - Lottospiel

--- SWP-Rubner/Lottospiel.py
+++ SWP-Rubner/Lottospiel.py
@@ -1,22 +1,29 @@
 import random
 
+# min = 50
+# max = 100
+# anz = 7 Ziehungen
+def zufall(min=0, max=45, anz=6):
+    list = [*range(min, max+1, 1)]
+    for i in range(anz):
+        rand = random.randrange(0, max-min+1)
+        lastposition = len(list)-1-i
+        list[rand], list[lastposition], = list[lastposition], list[rand]
+    return list[-anz:]
 
+def statistik1(min, max, anz, anz_ziehungen=1000):
+    stat_dic={}
+    for i in range(min, max+1):
+        stat_dic[i] = 0
+    for i in range(anz_ziehungen):
+        for zahl in zufall(min,max,anz):
+            stat_dic[zahl] += 1
+    return stat_dic
 
-
-def zufall():
-    list = [*range(1, 46, 1)]
-    arr = []
-    while len(arr) < 6:
-        rand = random.choice(list)
-        list.remove(rand)
-        arr.append(rand)
-    return arr
-
-def statistik():
+def statistik(min=0, max=45, anz=6, anzahl=1000):
     dic = {}
-    for i in range(1,1001):
-        arr = zufall()
-        for x in arr:
+    for i in range(anzahl):
+        for x in zufall(min, max, anz):
             if x in dic:
                 dic[x] = dic[x] + 1
             else:
@@ -24,9 +31,10 @@ def statistik():
     return dic
 
 if __name__ == '__main__':
-    #print(zufall())
+    print(zufall(0,15,22))
+    #print(statistik1(50,100,7))
 
-    for x, y in sorted(statistik().items()):
-        print(x, y)
+    #for x, y in sorted(statistik().items()):
+    #   print(x, y)
 
 
---
+++ SWP-Rubner/Poker.py
@@ -0,0 +1,142 @@
+import random
+import operator
+
+def model_poker(anz=5):
+    pokerlist = [*range(0, 52, 1)]
+    for i in range(anz):
+        rand = random.randrange(0, 52)
+        lastposition = len(pokerlist) - 1-i
+        pokerlist[rand], pokerlist[lastposition], = pokerlist[lastposition], pokerlist[rand]
+    for i in pokerlist[-anz:]:
+        pokerlist[pokerlist.index(i)] = symbol(i)
+    return pokerlist[-anz:]
+
+def symbol(zahl):
+    num = zahl % 13
+    n = num+1
+    if num == 10:
+        num = 'Bub'
+    elif num == 11:
+        num = 'Dame'
+    elif num == 12:
+        num = 'König'
+    elif num + 1 == 1:
+        num = 'Ass'
+    else:
+        num = num +1
+
+    sym = zahl // 13
+    if sym == 0:
+        sym = 'Pik'
+    elif sym == 1:
+        sym = 'Herz'
+    elif sym == 2:
+        sym = 'Karo'
+    else:
+        sym = 'Kreuz'
+
+    return  n, (str(num), sym)
+
+
+def statistik(anzahl=100000, anz=5):
+    dic = {}
+    for i in range(anzahl):
+            x = kombination(model_poker(anz))
+            if x in dic:
+                dic[x] = dic[x] + 1
+            else:
+                dic.update({x:1})
+    return dict( sorted(dic.items(), key=operator.itemgetter(1),reverse=True))
+
+
+
+
+def kombination(karten):
+    zahlen = []
+    for i in karten:
+        zahlen.append(i[0])
+    for i in karten:
+        k = i[0]
+
+        if zahlen.count(k) == 4:
+            return 'Four of a kind'
+        elif check_full_house(zahlen):
+            return 'Full House'
+        elif check_flush(karten):
+            return 'Flush'
+        elif check_straight(zahlen):
+            return 'Straight'
+        elif zahlen.count(k) ==3:
+            return 'Three of a kind'
+        elif check_two_pair(zahlen):
+            return 'Two pair'
+        elif zahlen.count(k) == 2:
+            return 'Pair'
+    return "High Card"
+
+def check_straight(karten):
+    dic = {}
+    for i in karten:
+        if i in dic:
+            dic[i] = dic[i] + 1
+        else:
+            dic.update({i: 1})
+    isStraight = max(karten) - min(karten)
+    k = len(set(dic.values()))
+    if(k ==1 and isStraight==4):
+        return True
+    elif set(karten) == set([1,13,12,11,10]):
+        return True
+    else:
+        return False
+
+def check_flush(karten):
+    dic = {}
+    for i in karten:
+        k = i[1][1]
+        if k in dic:
+            dic[k] = dic[k] + 1
+        else:
+            dic.update({k: 1})
+    if len(set(dic)) == 1:
+        return True
+
+    return False
+
+
+def check_two_pair(karten):
+    dic = {}
+    for i in karten:
+        if i in dic:
+            dic[i] = dic[i] + 1
+        else:
+            dic.update({i: 1})
+    if sorted(dic.values()) == [1,2,2]:
+        return True
+    else:
+        return False
+
+def check_full_house(karten):
+    dic = {}
+    for i in karten:
+        if i in dic:
+            dic[i] = dic[i] + 1
+        else:
+            dic.update({i: 1})
+    if sorted(dic.values()) == [2, 3]:
+        return True
+    else:
+        return False
+def gesamt_kombis(statistik):
+    for i in statistik:
+        print(i +' : ' + str(statistik[i]))
+
+
+if __name__ == '__main__':
+
+    gesamt_kombis(statistik())
+    #check_flush([(8, ('8', 'Herz')), (6, ('6', 'Herz')), (8, ('8', 'Herz')), (7, ('7', 'Herz')), (6, ('6', 'Herz'))])
+    #check_straight([1,13,11,12,10])
+    #check_full_house([1,1,1,3,3])
+    #print(model_poker())
+    #test()
